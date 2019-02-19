094f2071c6a95848cc0171cacd8b320240651660
krish <bhavish.daswani@gmail.com>
2019-02-19 22:25:43 +0800

    with code first commit

---
+++ headfirst/pg_14_99_bots/PhraseOMatic.java
@@ -0,0 +1,8 @@
+public class PhraseOMatic{
+    public static void main (String[] args) {
+        
+        String t = "shade credit nasty pencil";
+        String [] wordListOne = t.split(" ");
+        System.out.println(wordListOne);   
+    }
+}
